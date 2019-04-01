1077388052c3852934b52d8a65d92146485f82a4
hhyue <hhyue@ctrip.com>
2019-04-01 13:48:16 +0800

    add randsampler

--- sampler.go
+++ sampler.go
@@ -9,25 +9,22 @@ import (
 
 type RandSampler struct {
 	r     *rand.Rand
-	width uint32
-	ratio uint32
+	base  int32
+	ratio int32
 }
 
-func NewRandSampler(ratio float32, width uint32) (*RandSampler, error) {
+func NewRandSampler(ratio float64) (*RandSampler, error) {
 	if ratio < 0 || ratio > 1 {
 		return nil, errors.New(fmt.Sprintf("ratio %f invalid, must between 0 and 1", ratio))
-	}
-	if width == 0 {
-		return nil, errors.New("width should not be zero")
 	}
 	s := rand.NewSource(time.Now().UnixNano())
 	return &RandSampler{
 		r:     rand.New(s),
-		width: width,
-		ratio: uint32(float64(ratio) * float64(width)),
+		base:  10000,
+		ratio: int32(ratio * 10000),
 	}, nil
 }
 
 func (rs *RandSampler) Sample() bool {
-	return rs.r.Int63n(int64(rs.width)) < int64(rs.ratio)
+	return rs.r.Int63n(int64(rs.base)) < int64(rs.ratio)
 }
--- sampler_test.go
+++ sampler_test.go
@@ -7,29 +7,52 @@ import (
 func TestNewSampler(t *testing.T) {
 	var err error
 	type testNewParam struct {
-		ratio   float32
-		width   uint32
+		ratio   float64
 		success bool
 	}
 	params := []testNewParam{
-		testNewParam{-1, 1000, false},
-		testNewParam{0, 1000, true},
-		testNewParam{0.3, 1000, true},
-		testNewParam{1, 1000, true},
-		testNewParam{2, 1000, false},
-		testNewParam{0.5, 0, false},
-		testNewParam{0.5, 100000000, true},
+		testNewParam{-1, false},
+		testNewParam{0, true},
+		testNewParam{0.3, true},
+		testNewParam{1, true},
+		testNewParam{2, false},
+		testNewParam{0.5, true},
 	}
 	for _, p := range params {
-		if _, err = NewRandSampler(p.ratio, p.width); (err != nil && p.success) || (err == nil && !p.success) {
-			t.Errorf("param(ratio, width = %f, %d), success should be %v, but err = %v", p.ratio, p.width, p.success, err)
+		if _, err = NewRandSampler(p.ratio); (err != nil && p.success) || (err == nil && !p.success) {
+			t.Errorf("param(ratio= %f), success should be %v, but err = %v", p.ratio, p.success, err)
 		}
 	}
 }
 
 func TestRandSamplerCorrectness(t *testing.T) {
 	type Param struct {
-		ratio float32
-		width uint32
+		ratio   float64
+		loopcnt int64
+		shotcnt int64
+	}
+	params := []Param{
+		Param{0, 1000000, 0},
+		Param{1, 1000000, 1000000},
+		Param{0.2, 1000000, 200000},
+		Param{0.3, 1000000, 300000},
+		Param{0.7, 1000000, 700000},
+		Param{0.3, 1000000, 300000},
+	}
+	var i int64
+
+	for _, p := range params {
+		s, _ := NewRandSampler(p.ratio)
+		shotcnt := 0
+		for i = 0; i < p.loopcnt; i++ {
+			if s.Sample() {
+				shotcnt++
+			}
+		}
+		if float64(shotcnt) > float64(p.shotcnt)*1.01 || float64(shotcnt) < float64(p.shotcnt)*0.99 {
+			t.Errorf("param = %+v failed, actual shotcnt = %d, sampler = %+v", p, shotcnt, s)
+		} else {
+			t.Logf("param = %+v success, actual shotcnt = %d", p, shotcnt)
+		}
 	}
 }
